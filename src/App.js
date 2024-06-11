289dfe6eae92dc7378f705574aeadd0bb173f8b9
Marta Łoskot <loskotmarta@gmail.com>
2024-06-11 17:25:19 +0200

    useState advanced

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import { useState } from "react"
 
 const App = () => {
-  const [count, setCount] = useState(null)
-  console.log(count)
+  const [formData,setFormData] = useState({})
 
-  // function handleClick() {
-  //   setCount(count+1)
-  // }
+  const chandleChange = (e) => {
+    const name = e.target.name
+    const value = e.target.value
+    console.log(formData)
+    setFormData(
+      {
+        ...formData,
+        [name]: value
+      }
+    )
+  }
 
   return (
     <div>
-      <button onClick={() => setCount(count+1)}>add 1</button>
-    </div>
+   <form>
+    <input onChange={chandleChange} placeholder="first name" value={formData.firstName} name="firstName"></input>
+    <input onChange={chandleChange} placeholder="email" type="email" value={formData.email} name="email"></input>
+   </form>
+   </div>
   )
 }
 
