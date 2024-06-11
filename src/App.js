94683259cab9553e84a7d1f6be5e3d5360701422
Marta Łoskot <loskotmarta@gmail.com>
2024-06-11 15:12:43 +0200

    useState example

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { useState } from "react"
 
 const App = () => {
+  const [count, setCount] = useState(null)
+  console.log(count)
+
+  // function handleClick() {
+  //   setCount(count+1)
+  // }
+
   return (
-    <>
-    </>
+    <div>
+      <button onClick={() => setCount(count+1)}>add 1</button>
+    </div>
   )
 }
 
